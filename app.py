a47dca704bbdaf46ce1087fa9d513f101b2f397b
CaptainOddessy <109068144+CaptainOddessy@users.noreply.github.com>
2023-12-11 11:21:12 +0000

    add requirements

--- app.py
+++ app.py
@@ -1,47 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Python Developer',
+    'location': 'New York',
+    'salary': '$100,000'
+}, {
+    'id': 2,
+    'title': 'Software Engineer',
+    'location': 'London'
+}, {
+    'id': 3,
+    'title': 'Data Analyst',
+    'location': 'San Francisco',
+    'salary': '$80,000'
+}, {
+    'id': 4,
+    'title': 'Web Developer',
+    'location': 'Berlin',
+    'salary': '$90,000'
+}, {
+    'id': 5,
+    'title': 'Machine Learning Engineer',
+    'location': 'Bangalore',
+    'salary': '$150,000'
+}]
 
-JOBS =[
-    {
-        'id': 1,
-        'title': 'Python Developer',
-        'location': 'New York',
-        'salary': '$100,000'},
-    {
-      'id': 2,
-      'title': 'Software Engineer',
-      'location': 'London'
-      
-    },
-    {
-      'id': 3,
-      'title': 'Data Analyst',
-      'location': 'San Francisco',
-      'salary': '$80,000'
-    },
-    {
-      'id': 4,
-      'title': 'Web Developer',
-      'location': 'Berlin',
-      'salary': '$90,000'
-    },
-    {
-      'id': 5,
-      'title': 'Machine Learning Engineer',
-      'location': 'Bangalore',
-      'salary': '$150,000'
-    }
-
-]
 
 @app.route('/')
 def hello_world():
-  return render_template('index.html', 
-                         jobs = JOBS)
+  return render_template('index.html', jobs=JOBS)
+
+
+@app.route('/api/jobs')
+def new_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
-  
